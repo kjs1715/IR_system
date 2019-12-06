1fd5a2fb1f46e515b33b0ef456e72414e436aaa3
kjs1715 <kjsbuzz0506@gmail.com>
2019-12-06 18:47:41 +0800

    modified indexing methods

--- flask_app/indexer.py
+++ flask_app/indexer.py
@@ -6,10 +6,12 @@ from pathlib import Path
 from java.io import File
 from java.nio.file import Paths
 from org.apache.lucene.store import SimpleFSDirectory, FSDirectory
-from org.apache.lucene.document import Document, Field, FieldType
+from org.apache.lucene.document import Document, Field, FieldType, StringField
 from org.apache.lucene.analysis.cjk import CJKAnalyzer
 from org.apache.lucene.analysis.cn.smart import SmartChineseAnalyzer
 from org.apache.lucene.analysis.standard import StandardAnalyzer
+from org.apache.lucene.analysis import CharArraySet, TokenStream, tokenattributes
+from org.apache.lucene.analysis.core import WhitespaceAnalyzer
 from org.apache.lucene.index import IndexWriter, IndexWriterConfig, IndexOptions, IndexReader, DirectoryReader
 from org.apache.lucene.util import Version
 from org.apache.lucene.queryparser.classic import QueryParser
@@ -18,98 +20,141 @@ from org.apache.lucene.search import IndexSearcher, Query, PhraseQuery
 CORPUS_DIR = '/Users/kim/Desktop/corpus/'
 RAW_CORPUS_DIR = '/Users/kim/Desktop/raw_corpus/'
 SUFFIX = '_raw'
-LINE_LIMIT = 300000
+LINE_LIMIT = 100000
+
+stopwords_dic = [' ', '\\', '\\n', '\\np', '\\ns', '\\ni', '\\nz', '\\m', '\\m', '\\q', '\\mq', '\\t', '\\f', '\\s', '\\v', '\\a', '\\d', '\\h', '\\k', '\\i', '\\j', '\\r', '\\c', '\\p', '\\u', '\\y', '\\e', '\\o', '\\g', '\\w', '\\x']
 
 '''
-    Combine seperated terms into sentences
+	Combine seperated terms into sentences
 '''
 def combine_sentences(fileName):
-    line_count = 0
-    sentence = ''
+	line_count = 0
+	sentence = ''
 
-    with open(CORPUS_DIR + fileName, 'r') as f:
-        line = f.readline()
-        while(line):
-            line_count += 1
-            terms = line.split(' ')
-            for term in terms:
-                term = term.split('\\')[0]
-                sentence += term
-            sentence += '\n'
-            line = f.readline()
-    f.close()
-    with open(RAW_CORPUS_DIR + fileName + SUFFIX, 'w') as f1:
-        f1.write(sentence)
-    f1.close()
+	with open(CORPUS_DIR + fileName, 'r') as f:
+		line = f.readline()
+		while(line):
+			line_count += 1
+			terms = line.split(' ')
+			for term in terms:
+				term = term.split('/')[0]
+				sentence += term
+			sentence += '\n'
+			line = f.readline()
+	f.close()
+	with open(RAW_CORPUS_DIR + fileName + SUFFIX, 'w') as f1:
+		f1.write(sentence)
+	f1.close()
 '''
-    Convert all files into raw_text
+	Convert all files into raw_text
 '''
 def combine_files():
-    for file in ['Sogou0005', 'Sogou0007', 'Sogou0011', 'Sogou0010', 'Sogou0015', 'Sogou0017']:
-        start = time.time()
-        combine_sentences(file)
-        end = time.time()
-        print(file + ' completed, time : %f' % (end - start))
+	for file in ['Sogou0005', 'Sogou0007', 'Sogou0011', 'Sogou0010', 'Sogou0015', 'Sogou0017']:
+		start = time.time()
+		combine_sentences(file)
+		end = time.time()
+		print(file + ' completed, time : %f' % (end - start))
+
+'''
+	Indexing all corpus in Chinese with lucene
+'''
 
 def recover_sentence(terms):
-    sentence = ''
-    terms = terms.split(' ')
-    for term in terms:
-        term = term.split('\\')[0]
-        sentence += term
-    
-    return sentence
+	sentence = ''
+	terms = terms.split(' ')
+	for term in terms:
+		term = term.split('\\')[0]
+		sentence += term
+	
+	return sentence
+
+def test_analyzer():
+	analyzer = WhitespaceAnalyzer(Version.LATEST)
+	# tokenStream = analyzer.tokenStream('text', '')
 
 '''
-    Indexing all corpus in Chinese with lucene
+	Indexer constructed with Lucene
 '''
 class Indexer():
-    def __init__(self, path):
-        p = Path(path)
-        if not p.is_dir():
-            os.mkdir(path)
-        storeDir = SimpleFSDirectory(Paths.get(path))
-        # analyzer = CJKAnalyzer()
-        analyzer = SmartChineseAnalyzer()
-        config = IndexWriterConfig(analyzer)
-        index_writer = IndexWriter(storeDir, config)
-        self.IndexDocs(index_writer)
+	def __init__(self, path):
+		stopwords = self.AddStopWords()
+		print(stopwords)
 
-    def IndexDocs(self, writer):
-	start = time.time()
-        line_count = 0
-        raw_texts = []
-        texts = []
-        # for fileName in ['2_1','2_10','2_3']:
-        with open('/root/corpus/corpus/Sogou0017', 'r') as f:
-        # with open('/Users/kim/Desktop/corpus/rmrb2_10.txt', 'r') as f:
-            # f1 = open('/Users/kim/Desktop/corpus/rmrb2_10.txt', 'r')
-	    for line in f:
-                line_count += 1
+		p = Path(path)
+		if not p.is_dir():
+			os.mkdir(path)
+		storeDir = SimpleFSDirectory(Paths.get(path))
+		# analyzer = StandardAnalyzer()
+		# analyzer = SmartChineseAnalyzer(stopwords)
+		analyzer = WhitespaceAnalyzer(Version.LATEST)
+		config = IndexWriterConfig(analyzer)
+		config.setOpenMode(IndexWriterConfig.OpenMode.CREATE_OR_APPEND)
+		index_writer = IndexWriter(storeDir, config)
+		self.IndexDocs(index_writer)
 
-                # field(raw)
-                fieldtype = FieldType()
-                fieldtype.setIndexOptions(IndexOptions.DOCS_AND_FREQS_AND_POSITIONS)
-                fieldtype.setStored(True)
-                # fieldtype1.setTokenized(True)
+	def IndexDocs(self, writer):
+		start = time.time()
+		line_count = 0
+		# for fileName in ['2_1','2_10','2_3']:
+		# with open('/root/corpus/corpus/Sogou0017', 'r') as f:
+		
+		# f1 = open(CORPUS_DIR + 'rmrb0010', 'r')
+		with open(CORPUS_DIR + 'Sogou0017', 'r') as f:
+			for line in f:
+				terms, phrases = self.split_phrase(line)
+				line_count += 1
+				# token_line = f1.readline()
 
-                # field(result)
-                # fieldtype2 = FieldType()
-                #fieldtype2.setStored(True)
+				# field(raw)
+				fieldtype = FieldType()
+				fieldtype.setIndexOptions(IndexOptions.DOCS_AND_FREQS_AND_POSITIONS)
+				fieldtype.setStored(True)
+				fieldtype.setTokenized(True)
 
-                doc = Document()
-                # doc.add(Field('raw_text', recover_sentence(line), fieldtype1))
-                doc.add(Field('text', line, fieldtype))
+				# field(tokenized)
+				fieldtype2 = FieldType()
+				fieldtype2.setStored(True)
 
-                writer.addDocument(doc)
-                # if line_count % LINE_LIMIT == 0:
-                #     break
-        writer.close()
-        print('indexing completed')
-        end = time.time()
-        print('time: %f' % (end - start))
-        print(line_count)
-        line_count = 0
-        f.close()
+				doc = Document()
+				doc.add(Field('text', terms, fieldtype))
+				doc.add(Field('phrase', phrases, fieldtype2))
 
-        
+				writer.addDocument(doc)
+				# if line_count % LINE_LIMIT == 0:
+				# 	break
+		writer.close()
+		print('indexing completed')
+		end = time.time()
+		print('time: %f' % (end - start))
+		print(line_count)
+		line_count = 0
+		f.close()
+
+	def AddStopWords(self):
+		count = 0
+		f1 = open('stopwords1.txt', 'r' )
+		f2 = open('stopwords2.txt', 'r')
+		array_set = CharArraySet(2690, True)
+		for word in f1:
+			word = word.strip('\n')
+			array_set.add(word)
+		for word in f2:
+			array_set.add(word)
+		for word in stopwords_dic:
+				array_set.add(word)
+		f1.close()
+		f2.close()
+
+		return array_set
+
+	def split_phrase(self, sentence):
+		term_combine = ''
+		phrase_combine = ''
+		terms = sentence.split(' ')
+		for term in terms:
+			temp = term.split('/')
+			term_combine += temp[0] + ' '
+			phrase_combine += temp[1] + ' '
+		
+		return term_combine, phrase_combine
+		
--- flask_app/retriever.py
+++ flask_app/retriever.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import lucene
+import thulac
 from pathlib import Path
 from java.io import File
 from java.nio.file import Paths
@@ -9,7 +10,8 @@ from org.apache.lucene.document import Document, Field, FieldType
 from org.apache.lucene.analysis.cjk import CJKAnalyzer
 from org.apache.lucene.analysis.cn.smart import SmartChineseAnalyzer
 from org.apache.lucene.analysis.standard import StandardAnalyzer
-from org.apache.lucene.index import IndexWriter, IndexWriterConfig, IndexOptions, IndexReader, DirectoryReader
+from org.apache.lucene.analysis.core import WhitespaceAnalyzer
+from org.apache.lucene.index import IndexWriter, IndexWriterConfig, IndexOptions, IndexReader, DirectoryReader, Term
 from org.apache.lucene.util import Version
 from org.apache.lucene.queryparser.classic import QueryParser
 from org.apache.lucene.search import IndexSearcher, Query, PhraseQuery
@@ -19,56 +21,103 @@ prefixHTML = u"<font color='red'>"
 suffixHTML = u"</font>"
 
 '''
-    Retrieving query in indexed file, return results
+	Retrieving query in indexed file, return results
 '''
 class Retriever():
-    # __init__ should not return value
-    hits = []
-    path = ''
-    def __init__(self, path):
-        print('Searcher initialized...')
-        self.path = path
+	# __init__ should not return value
+	hits = []
+	path = ''
+	analyzer = ''
+	reader = ''
+	searcher = ''
+	thu = ''
+	def __init__(self, path):
+		print('Searcher initialized...')
+		self.path = path
+		self.analyzer = SmartChineseAnalyzer()
+		# self.analyzer = WhitespaceAnalyzer(Version.LATEST)
+		self.reader = DirectoryReader.open(SimpleFSDirectory(Paths.get(self.path)))
+		self.searcher = IndexSearcher(self.reader)
+		self.thu = thulac.thulac(deli='/')
 
 
-    def search(self, term):
-        self.hits = []
+	def search(self, term):
+		self.hits = []
+		indexreader = self.searcher.getIndexReader()
+		# print(indexreader.document(10))
+		parser = QueryParser('text', self.analyzer)
+		query = parser.parse(term)
 
-        analyzer = SmartChineseAnalyzer()
-        reader = DirectoryReader.open(SimpleFSDirectory(Paths.get(self.path)))
-        searcher = IndexSearcher(reader)
-        indexreader = searcher.getIndexReader()
-        print(indexreader.numDocs())
-        parser = QueryParser('text', analyzer)
-        query = parser.parse(term)
+		hits = self.searcher.search(query, 20).scoreDocs
 
-        hits = searcher.search(query, 20).scoreDocs
+		for hit in hits:
+			doc = self.searcher.doc(hit.doc)
+			text = doc.get("text")
+			phrase = doc.get('phrase')
+			simpleHTMLFormatter = SimpleHTMLFormatter(prefixHTML, suffixHTML)
+			highlighter = Highlighter(simpleHTMLFormatter, QueryScorer(query))
+			# highLightText = highlighter.getBestFragment(analyzer, 'text', self.recover_sentence(doc.get('text')))
+			highLightText = highlighter.getBestFragment(self.analyzer, 'text', self.recover_sentence(text))
+			print(hit.score, hit.doc, hit.toString())
+			print(text)
+			print(phrase)
+			self.hits.append(highLightText)
+			
+		print('search over')
+		# reader.close()
+		return self.hits
 
-        for hit in hits:
-            doc = searcher.doc(hit.doc)
-            simpleHTMLFormatter = SimpleHTMLFormatter(prefixHTML, suffixHTML)
-            highlighter = Highlighter(simpleHTMLFormatter, QueryScorer(query))
-            highLightText = highlighter.getBestFragment(analyzer, 'text', recover_sentence(doc.get('text')))
-            print(hit.score, hit.doc, hit.toString())
-            print(highLightText)
-            text = doc.get("text")
-            print(text)
-            self.hits.append(highLightText)
-            
-        print('search over')
-        reader.close()
-        return self.hits
+	def search_phrase(self, term, phrase):
+		self.hits = []
+		parser = QueryParser('text', self.analyzer)
+		query = parser.parse(term)   
 
-    def search_phrase(self):
-        self.hits = []
+		hits = self.searcher.search(query, 1000).scoreDocs
+		if hits is None:
+			return 
 
-    def recover_sentence(self, terms):
-        sentence = ''
-        terms = terms.split(' ')
-        for term in terms:
-            term = term.split('\\')[0]
-            sentence += term
-        
-        return sentence
+		for hit in hits:
+			doc = self.searcher.doc(hit.doc)
+			text = doc.get("text")
+			token_text = doc.get("token_text")
+			simpleHTMLFormatter = SimpleHTMLFormatter(prefixHTML, suffixHTML)
+			highlighter = Highlighter(simpleHTMLFormatter, QueryScorer(query))
+			highLightText = highlighter.getBestFragment(analyzer, 'text', self.recover_sentence(doc.get('text')))
+			# highLightText = highlighter.getBestFragment(self.analyzer, 'text', text)
 
+			token_terms = token_text.split(' ')
+			for token_term in token_terms:
+				token = token_term.split('/')
+				if token[0] == term:
+					if token[1] == phrase:
+						print('append %s' % term)
+						self.hits.append(highLightText)
+		return self.hits[:20]
+
+	'''
+		Using thulac for marking phrase of query
+	'''
+	def mark_term(self, query):
+		marked_term = self.thu.cut(query, text=True)
+		term_sentence = ''
+		print(marked_term)
+		if len(marked_term) > 1:
+			for term in marked_term:
+				term_sentence += term
+		else:
+			term_sentence = marked_term[0]
+		return term_sentence
+		
+	'''
+		convert words into sentence
+	'''
+	def recover_sentence(self, terms):
+		print(terms)
+		sentence = ''
+		terms = terms.split(' ')
+		print(terms)
+		for term in terms:
+			sentence += term
+		return sentence
 
 
